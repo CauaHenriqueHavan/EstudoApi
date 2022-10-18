e768a8587a7355a69bd3bdbfb62c1d8d8d70e41f
Marcelo Luiz <marcelo_luiz17@outlook.com>
2022-10-18 17:06:05 -0300

    Resolvendo conflito de merge

--- estudo.domain/Interfaces/Repository/IClienteRepository.cs
+++ estudo.domain/Interfaces/Repository/IClienteRepository.cs
@@ -1,11 +1,16 @@
-﻿namespace estudo.domain.Interfaces.Repository
+﻿using estudo.domain.DTO_s;
+using estudo.domain.DTO_s.InputModels;
+using estudo.domain.DTO_s.OutPutModelAuxiliar;
+using estudo.domain.Entities;
+
+namespace estudo.domain.Interfaces.Repository
 {
     public interface IClienteRepository
     {
         Task<ClienteEntity> CriarClienteAsync(CadastrarClienteInputModel model);
         Task<PaginadoOutputModel<ClienteOutputModel>> BuscarClientesAsync(BuscarClientesInputModel model);
         Task<ClienteOutputModel> BuscarClientesIdAsync(short id);
-        Task<bool> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model);
+        Task<ClienteEntity> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model);
 
         Task<ClienteEntity> AlterarSituacaoClientesAsync(short id);
     }
