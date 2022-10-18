30acf36deb91dc1364131086ae6d148066cee225
Marcelo Luiz <marcelo_luiz17@outlook.com>
2022-10-18 15:34:42 -0300

    Implementando ResultViewBaseModel

--- estudo.domain/Interfaces/Service/IClienteService.cs
+++ estudo.domain/Interfaces/Service/IClienteService.cs
@@ -1,14 +1,16 @@
-﻿using estudo.domain.DTO_s;
+﻿using estudo.domain.Auxiliar;
+using estudo.domain.DTO_s;
 using estudo.domain.DTO_s.InputModels;
 
 namespace estudo.domain.Interfaces.Service
 {
     public interface IClienteService
     {
-        Task<bool> CriarClienteAsync(CadastrarClienteInputModel model);
-        Task<List<ClienteOutputModel>> BuscarClientesAsync();
-        Task<ClienteOutputModel> BuscarClientesIdAsync(short id);
-        Task<bool> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model);
-        Task<bool> AlterarSituacaoClienteAsync(short id);
+        Task<ResultViewBaseModel> CriarClienteAsync(CadastrarClienteInputModel model);
+        Task<ResultViewBaseModel> BuscarClientesAsync();
+        Task<ResultViewBaseModel> BuscarClientesIdAsync(short id);
+        Task<ResultViewBaseModel> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model);
+        Task<ResultViewBaseModel> AlterarSituacaoClienteAsync(short id);
     }
 }
+
--- estudo.service/ClienteService.cs
+++ estudo.service/ClienteService.cs
@@ -1,4 +1,4 @@
-﻿using estudo.domain.DTO_s;
+﻿using estudo.domain.Auxiliar;
 using estudo.domain.DTO_s.InputModels;
 using estudo.domain.Interfaces.Repository;
 using estudo.domain.Interfaces.Service;
@@ -6,7 +6,7 @@ using estudo.infra.Context;
 
 namespace estudo.service
 {
-    public class ClienteService : IClienteService
+    public class ClienteService : BaseService, IClienteService
     {
         private readonly IClienteRepository _clienteRepository;
         private readonly IUnitOfWork<AppDbContext> _uow;
@@ -18,31 +18,40 @@ namespace estudo.service
         }
             
 
-        public async Task<List<ClienteOutputModel>> BuscarClientesAsync()
-            => await _clienteRepository.BuscarClientesAsync();
+        public async Task<ResultViewBaseModel> BuscarClientesAsync()
+            => AddResult(await _clienteRepository.BuscarClientesAsync());
 
-        public async Task<ClienteOutputModel> BuscarClientesIdAsync(short id)
-            => await _clienteRepository.BuscarClientesIdAsync(id);
+        public async Task<ResultViewBaseModel> BuscarClientesIdAsync(short id)
+            => AddResult(await _clienteRepository.BuscarClientesIdAsync(id));
 
-        public async Task<bool> CriarClienteAsync(CadastrarClienteInputModel model)
-            => await _clienteRepository.CriarClienteAsync(model);
+        public async Task<ResultViewBaseModel> CriarClienteAsync(CadastrarClienteInputModel model)
+            => AddResult(await _clienteRepository.CriarClienteAsync(model));
 
-        public async Task<bool> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model)
-            => await _clienteRepository.AlterarCadastroClienteAsync(model);
+        public async Task<ResultViewBaseModel> AlterarCadastroClienteAsync(AlterarCadastroClienteInputModel model)
+        {
+            var cliente  = await _clienteRepository.AlterarCadastroClienteAsync(model);
+            if(cliente == false)
+                return AddErros(ResourceService.ClienteNaoEncontrado);
 
-        public async Task<bool> AlterarSituacaoClienteAsync(short id)
+            await _uow.CommitAsync();
+
+            return AddResult(true);
+        }
+           
+
+        public async Task<ResultViewBaseModel> AlterarSituacaoClienteAsync(short id)
         {
             if (id <= 0)
-                return false;
+                return AddErros(ResourceService.IdInvalido);
 
             var cliente = await _clienteRepository.AlterarSituacaoClientesAsync(id);
 
             if (cliente == null)
-                return false;
+                return AddErros(ResourceService.ClienteNaoEncontrado);
 
             await _uow.CommitAsync();
 
-            return true;
+            return AddResult(true);
         }
     }
 }
